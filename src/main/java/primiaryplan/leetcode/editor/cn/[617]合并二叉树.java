763242958abbb1739d19c8f34e5ba08434bd5f85
SAM-COODE <lishimincorrect@gmail.com>
2021-09-23 20:08:52 +0800

    合并二叉树

--- src/main/java/primiaryplan/leetcode/editor/cn/[617]合并二叉树.java
+++ src/main/java/primiaryplan/leetcode/editor/cn/[617]合并二叉树.java
@@ -29,6 +29,7 @@ package primiaryplan.leetcode.editor.cn;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -45,7 +46,7 @@ package primiaryplan.leetcode.editor.cn;
  * }
  */
 
-class Solution {
+class Solution617 {
 
     public static class TreeNode {
         int val;
@@ -61,7 +62,16 @@ class Solution {
     }
 
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        return null;
+        if(root1==null){
+            return root2;
+        }
+        if(root2==null){
+            return root1;
+        }
+        TreeNode root3 = new TreeNode(root1.val+root2.val);
+        root3.left = mergeTrees(root1.left,root2.left);
+        root3.right = mergeTrees(root1.right,root2.right);
+        return root3;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
