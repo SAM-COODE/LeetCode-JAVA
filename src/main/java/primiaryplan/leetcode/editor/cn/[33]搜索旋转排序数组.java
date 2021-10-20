682ad0b170c0076db549995005deb562d387d403
SAM-COODE <lishimincorrect@gmail.com>
2021-10-20 17:21:00 +0800

    哦豁

--- src/main/java/primiaryplan/leetcode/editor/cn/[33]搜索旋转排序数组.java
+++ src/main/java/primiaryplan/leetcode/editor/cn/[33]搜索旋转排序数组.java
@@ -50,6 +50,36 @@ package primiaryplan.leetcode.editor.cn;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution33 {
     public int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    public int NotTooFoolSearch(int[] nums, int target) {
         //首先找出旋转点的位置,再判断属于哪一个分组进行二分查找
         //共两次二分
         int length = nums.length;
